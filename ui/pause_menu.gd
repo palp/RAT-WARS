b0e5c19b3bcec04e22ade989e6931d821bb890b0
GonnerMB <adrianfelber99@gmail.com>
2023-11-26 20:07:54 +0100

    split main menu and pause menu into separate scenes

--- ui/pause_menu.gd
+++ ui/pause_menu.gd
@@ -2,8 +2,9 @@ extends Control
 
 @onready var options_menu = get_node("%options_menu")
 
-func _on_play_pressed():
-	get_tree().change_scene_to_file("res://level_placeholder.tscn")
-
 func _on_options_pressed():
 	options_menu.visible = !options_menu.visible
+
+func _on_main_menu_pressed():
+	get_tree().change_scene_to_file("res://demo.tscn")
+	get_tree().paused = false
