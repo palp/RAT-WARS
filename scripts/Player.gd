72864e3cbea6aee2ea2f7023f6f38515dec3d6e0
Stephan Auerhahn <palp@palp.org>
2023-11-26 08:16:30 -0800

    add basic autopilot mode to player (#10)
    * add basic autopilot mode to player
    
    * Better autopilot, add tests
    
    * Merge branch 'autopilot' of github.com:palp/RAT-WARS into autopilot
    
    * Fix tests outside engine by adding yield
    
    * Add gdunit4, tests, CI (#11)
    
    * Add gdunit4, tests, CI
    
    * remove GUT tests
    
    * add permissions
    
    * simulate frames
    
    * simulate more frames
    
    * more frames
    
    * fixed player tests
    
    * disable GUT
    
    * formatting
    
    * test y axis specifically

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,9 +1,35 @@
 extends CharacterBody2D
 
 @export var speed = 200
+@export var autopilot = true
+
 
 func _physics_process(delta):
-	var dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = dir.normalized() * speed
-	
+	if autopilot:
+		var enemies = get_tree().get_nodes_in_group("enemy")
+		var dir = velocity.normalized()
+		velocity = get_autopilot_velocity(dir, enemies)
+	else:
+		velocity = get_input_velocity()
+
 	move_and_slide()
+
+
+func get_autopilot_velocity(dir: Vector2, enemies: Array):
+	for enemy in enemies:
+		var enemy_distance = enemy.global_position - global_position
+		var enemy_distance_total = abs(enemy_distance.x) + abs(enemy_distance.y)
+		if enemy_distance_total < 100:
+			var enemy_dir = enemy.global_position.direction_to(global_position)
+			dir += enemy_dir * 0.5
+
+	if dir == Vector2(0, 0) or randf() < 0.05:
+		var random_factor = Vector2(randf_range(-0.5, 0.5), randf_range(-0.5, 0.5))
+		dir += random_factor
+
+	return dir.normalized() * speed
+
+
+func get_input_velocity():
+	var dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	return dir.normalized() * speed
