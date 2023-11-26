128a1d20708e2c782e6135519b2c3b7572922180
Stephan Auerhahn <stephan@stability.ai>
2023-11-26 08:08:46 -0800

    formatting

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @export var speed = 200
 @export var autopilot = true
 
+
 func _physics_process(delta):
 	if autopilot:
 		var enemies = get_tree().get_nodes_in_group("enemy")
@@ -14,19 +15,20 @@ func _physics_process(delta):
 	move_and_slide()
 
 
-func get_autopilot_velocity(dir:Vector2, enemies:Array):
-	for enemy in enemies:		
+func get_autopilot_velocity(dir: Vector2, enemies: Array):
+	for enemy in enemies:
 		var enemy_distance = enemy.global_position - global_position
-		var enemy_distance_total = abs(enemy_distance.x) + abs(enemy_distance.y)		
-		if (enemy_distance_total < 100):
+		var enemy_distance_total = abs(enemy_distance.x) + abs(enemy_distance.y)
+		if enemy_distance_total < 100:
 			var enemy_dir = enemy.global_position.direction_to(global_position)
-			dir += enemy_dir * 0.5			
+			dir += enemy_dir * 0.5
 
-	if dir == Vector2(0,0) or randf() < 0.05:
+	if dir == Vector2(0, 0) or randf() < 0.05:
 		var random_factor = Vector2(randf_range(-0.5, 0.5), randf_range(-0.5, 0.5))
 		dir += random_factor
 
 	return dir.normalized() * speed
+
 
 func get_input_velocity():
 	var dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
