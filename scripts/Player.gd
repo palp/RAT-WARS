1c93c97a5be934b9d72f5c558a87ffd6e15adc9d
wolfpaper <134673381+wolf-paper@users.noreply.github.com>
2023-11-27 02:24:00 -0500

    Implementing Rat Wars Vinyl weapon and beginning work on Buttplug (#20)
    * initialiing personal branch
    
    * beginning work on vinyl and plug weapons
    
    * Implementing vinyl spawning and pathing
    TODO: handle enemy colision and enemy despawning, upgrades

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -10,7 +10,24 @@ var invincibility = 0
 @export var speed = 200
 @export var autopilot = true
 
+# Weapon/Attack variables
+var vinyl = preload("res://vinyl.tscn")
+@onready var VinylTimer = get_node("%VinylTimer")
+@onready var VinylAttackTimer = get_node("%VinylAttackTimer")
+
+# Vinyl Specific
+var vinyl_ammo = 0
+var vinyl_base_ammo = 1
+var vinyl_attack_speed = 3
+var vinyl_level = 1
+
+# Enemies detected
+var enemy_close = []
+
 const Utility = preload("res://scripts/utility.gd")
+
+func _ready():
+	attack()
 
 func _physics_process(delta):
 	if health <= 0:
@@ -64,3 +81,44 @@ func get_autopilot_velocity(dir: Vector2, enemies: Array):
 func get_input_velocity():
 	var dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	return dir.normalized() * speed
+
+func attack():
+	if vinyl_level > 0:
+		VinylTimer.wait_time = vinyl_attack_speed
+		if VinylTimer.is_stopped():
+			VinylTimer.start()
+
+
+func _on_vinyl_timer_timeout():
+	vinyl_ammo += vinyl_base_ammo
+	VinylAttackTimer.start()
+
+
+func _on_vinyl_attack_timer_timeout():
+	if vinyl_ammo > 0:
+		var vinyl_attack = vinyl.instantiate()
+		vinyl_attack.position = position
+		vinyl_attack.level = vinyl_level
+		vinyl_attack.target = get_random_target()
+		add_child(vinyl_attack)
+		vinyl_ammo -= 1
+		if vinyl_ammo > 0:
+			VinylAttackTimer.start()
+		else:
+			VinylAttackTimer.stop()
+		
+func get_random_target():
+	if enemy_close.size() > 0:
+		return enemy_close.pick_random().global_position
+	else:
+		return Vector2.UP
+
+
+func _on_enemy_detection_area_body_entered(body):
+	if not enemy_close.has(body):
+		enemy_close.append(body)
+
+
+func _on_enemy_detection_area_body_exited(body):
+	if enemy_close.has(body):
+		enemy_close.erase(body)
