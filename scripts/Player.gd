683c2456c8f2d070e3c312a135b28723fa3734c0
Stephan Auerhahn <palp@palp.org>
2023-11-28 00:02:31 -0800

    Move autopilot code into state machine and update test (#28)

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -18,11 +18,6 @@ func _physics_process(delta):
 	if health <= 0:
 		return
 
-	if autopilot:
-		var enemies = get_tree().get_nodes_in_group("enemy")
-		var dir = velocity.normalized()
-		velocity = get_autopilot_velocity(dir, enemies)
-
 	move_and_slide()
 	Utility.set_facing(self)
 	var sprite = get_node("Sprite2D")
@@ -45,21 +40,6 @@ func _physics_process(delta):
 	#for i in get_slide_collision_count():
 	#	var collision = get_slide_collision(i)
 	#	print("I collided with ", collision.get_collider().damage)
-
-
-func get_autopilot_velocity(dir: Vector2, enemies: Array):
-	for enemy in enemies:
-		var enemy_distance = enemy.global_position - global_position
-		var enemy_distance_total = abs(enemy_distance.x) + abs(enemy_distance.y)
-		if enemy_distance_total < 100:
-			var enemy_dir = enemy.global_position.direction_to(global_position)
-			dir += enemy_dir * 0.5
-
-	if dir == Vector2(0, 0) or randf() < 0.05:
-		var random_factor = Vector2(randf_range(-0.5, 0.5), randf_range(-0.5, 0.5))
-		dir += random_factor
-
-	return dir.normalized() * speed
 
 #func get_input_velocity():
 #	var dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
--- test/scripts/AutopilotTest.gd
+++ test/scripts/AutopilotTest.gd
@@ -1,26 +1,29 @@
 # GdUnit generated TestSuite
-class_name PlayerTest
+class_name AutopilotTest
 extends GdUnitTestSuite
 @warning_ignore("unused_parameter")
 @warning_ignore("return_value_discarded")
 # TestSuite generated from
-const __source = "res://scripts/Player.gd"
+const __source = "res://scripts/StateMachine/Autopilot.gd"
+
+var autopilot
+
+
+func before_test():
+	autopilot = auto_free(load(__source).new())
+	autopilot.player = mock(Player, CALL_REAL_FUNC)
 
 
 func test_get_autopilot_velocity_no_enemies() -> void:
-	var player = load(__source).new()
-	auto_free(player)
-	var velocity = player.get_autopilot_velocity(Vector2(0, 0), Array())
+	var velocity = autopilot.get_autopilot_velocity(Vector2(0, 0), Array())
 	assert_vector2(velocity).is_not_equal(Vector2(0, 0))
 
 
 func test_get_autopilot_velocity_one_enemy() -> void:
-	var player = load(__source).new()
-	auto_free(player)
 	var enemies = Array()
 	var enemy_1 = Node2D.new()
 	auto_free(enemy_1)
 	enemy_1.global_position = Vector2(0, 0.1)
 	enemies.append(enemy_1)
-	var velocity = player.get_autopilot_velocity(Vector2(0, 0), enemies)
+	var velocity = autopilot.get_autopilot_velocity(Vector2(0, 0), enemies)
 	assert_float(velocity.y).is_less(0)
