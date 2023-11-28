090853a4cf046285ce732ce011a611695518f174
Luke <larson.luke.t@gmail.com>
2023-11-28 00:07:26 -0600

    Camera soulsified (#26)
    Camera now lags slightly behind the player, and will catch up when movement stops.
    
    Additionally, fixed an issue caused by the state machine that was allowing arrow key inputs while in main menu autopilot mode.

---
+++ Autopilot.gd
@@ -0,0 +1,8 @@
+extends PlayerState
+
+func enter(_msg := {}) -> void:
+	return
+
+func physics_update(delta: float) -> void:
+	if (player.autopilot == false):
+		state_machine.transition_to("Idle")
