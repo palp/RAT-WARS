fbc0f25d0449bd1e2acb89ff59a5efac815f567a
Treb Connell <trebconnell@gmail.com>
2023-11-29 00:12:36 -0800

    Merge tutorial project into our project (#37)
    Co-authored-by: Treb Connell <trebconnell@Trebs-MacBook-Pro.local>

---
+++ Player/Attack/vinyl.gd
@@ -0,0 +1,67 @@
+extends Area2D
+
+var level = 1
+var hp = 999
+var speed = 150.0
+var damage = 10
+var attack_size = 1.0
+var knockback_amount = 100
+
+var target = Vector2.ZERO
+
+var angle = Vector2.ZERO
+var reset_pos = Vector2.ZERO
+
+#var sprt = preload("res://assets/player/attacks/vinyl.png")
+
+@onready var player = get_tree().get_first_node_in_group("player")
+@onready var sprite = $Sprite2D
+@onready var collision = $CollisionShape2D
+@onready var changeDirectionTimer = get_node("ChangeDirection")
+@onready var resetPost = get_node("ResetPosTimer")
+
+signal remove_from_array(object)
+
+
+func _ready():
+	angle = global_position.direction_to(target)
+	match level:
+		1:
+			hp = 9999
+			speed = 150.0
+			damage = 10
+			knockback_amount = 100
+			attack_size = 1.0
+		2:
+			hp = 9999
+			speed = 100.0
+			damage = 15
+			knockback_amount = 100
+			attack_size = 1.0
+		3:
+			hp = 9999
+			speed = 100.0
+			damage = 20
+			knockback_amount = 100
+			attack_size = 1.0
+		4:
+			hp = 9999
+			speed = 100.0
+			damage = 25
+			knockback_amount = 125
+			attack_size = 1.0
+
+func _physics_process(delta):
+	if angle.angle() >= 0:
+		rotation += .1
+	if angle.angle() < 0:
+		rotation -= .1
+	position += angle.normalized() * speed * delta
+
+func _on_flight_duration_timeout():
+	if level < 8:
+		queue_free()
+
+func _on_change_direction_timeout():
+	angle = angle.rotated(PI)
+	angle = angle.normalized()
---
+++ Player/player.gd
@@ -0,0 +1,444 @@
+class_name Player
+extends CharacterBody2D
+
+@export var autopilot = true
+var disable_pathing_input = false
+
+@export var movement_speed = 40.0
+@export var maxhp = 80
+var hp = maxhp
+var last_movement = Vector2.UP
+var time = 0
+
+var experience = 0
+var experience_level = 1
+var collected_experience = 0
+
+#Attacks
+var iceSpear = preload("res://Player/Attack/ice_spear.tscn")
+var tornado = preload("res://Player/Attack/tornado.tscn")
+var vinyl = preload("res://Player/Attack/vinyl.tscn")
+var javelin = preload("res://Player/Attack/javelin.tscn")
+
+#AttackNodes
+@onready var iceSpearTimer = get_node("%IceSpearTimer")
+@onready var iceSpearAttackTimer = get_node("%IceSpearAttackTimer")
+@onready var tornadoTimer = get_node("%TornadoTimer")
+@onready var tornadoAttackTimer = get_node("%TornadoAttackTimer")
+@onready var vinylTimer = get_node("%VinylTimer")
+@onready var vinylAttackTimer = get_node("%VinylAttackTimer")
+@onready var javelinBase = get_node("%JavelinBase")
+
+#UPGRADES
+var collected_upgrades = []
+var upgrade_options = []
+var armor = 0
+var speed = 0
+var spell_cooldown = 0
+var spell_size = 0
+var additional_attacks = 0
+
+#IceSpear
+var icespear_ammo = 0
+var icespear_baseammo = 0
+var icespear_attackspeed = 1.5
+var icespear_level = 0
+
+#Tornado
+var tornado_ammo = 0
+var tornado_baseammo = 0
+var tornado_attackspeed = 3
+var tornado_level = 0
+
+#Vinyl
+var vinyl_ammo = 0
+var vinyl_baseammo = 0
+var vinyl_attackspeed = 3
+var vinyl_level = 0
+
+#Javelin
+var javelin_ammo = 0
+var javelin_level = 0
+
+
+#Enemy Related
+var enemy_close = []
+
+
+@onready var sprite = $Sprite2D
+@onready var walkTimer = get_node("%walkTimer")
+
+#GUI
+@onready var expBar = get_node("%ExperienceBar")
+@onready var lblLevel = get_node("%lbl_level")
+@onready var levelPanel = get_node("%LevelUp")
+@onready var upgradeOptions = get_node("%UpgradeOptions")
+@onready var itemOptions = preload("res://Utility/item_option.tscn")
+@onready var sndLevelUp = get_node("%snd_levelup")
+@onready var healthBar = get_node("%HealthBar")
+@onready var lblTimer = get_node("%lblTimer")
+@onready var collectedWeapons = get_node("%CollectedWeapons")
+@onready var collectedUpgrades = get_node("%CollectedUpgrades")
+@onready var itemContainer = preload("res://Player/GUI/item_container.tscn")
+
+@onready var deathPanel = get_node("%DeathPanel")
+@onready var lblResult = get_node("%lbl_Result")
+@onready var sndVictory = get_node("%snd_victory")
+@onready var sndLose = get_node("%snd_lose")
+
+#Signal
+signal playerdeath
+
+
+# These could be abstracted to an input manager
+func check_movement_input():
+	return (
+		Input.is_action_just_pressed("ui_up")
+		or Input.is_action_just_pressed("ui_down")
+		or Input.is_action_just_pressed("ui_right")
+		or Input.is_action_just_pressed("ui_left")
+	)
+
+const FACING_EPSILON = 0.1
+
+static func set_facing(obj):
+	# Setting scale doesn't work so we do this slightly
+	# more complicsted logic.
+	if obj == null:
+		return
+	var sprite = obj.get_node("Sprite2D")
+	if sprite == null:
+		return
+	var base_scale = abs(obj.scale.x)
+	var new_flip = false
+	if obj.velocity.x < -FACING_EPSILON:
+		new_flip = true
+	elif obj.velocity.x > FACING_EPSILON:
+		new_flip = false
+	else:
+		# Just keep previous flip value
+		return
+
+	# Also flip offset if needed
+	if new_flip != sprite.is_flipped_h():
+		sprite.set_flip_h(new_flip)
+		sprite.offset.x = -sprite.offset.x
+
+func check_pathing_input():
+	return not disable_pathing_input and Input.is_action_pressed("click") 
+
+func get_movement_vector():
+	return Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+
+func get_pathing_target():
+	return get_global_mouse_position()
+
+func _ready():
+	upgrade_character("vinyl1")
+	attack()
+	set_expbar(experience, calculate_experiencecap())
+	_on_hurt_box_hurt(0,0,0)
+
+func _physics_process(delta):
+	pass
+
+func attack():
+	if icespear_level > 0:
+		iceSpearTimer.wait_time = icespear_attackspeed * (1-spell_cooldown)
+		if iceSpearTimer.is_stopped():
+			iceSpearTimer.start()
+	if tornado_level > 0:
+		tornadoTimer.wait_time = tornado_attackspeed * (1-spell_cooldown)
+		if tornadoTimer.is_stopped():
+			tornadoTimer.start()
+	if vinyl_level > 0:
+		vinylTimer.wait_time = vinyl_attackspeed * (1-spell_cooldown)
+		if vinylTimer.is_stopped():
+			vinylTimer.start()
+	if javelin_level > 0:
+		spawn_javelin()
+
+func _on_hurt_box_hurt(damage, _angle, _knockback):
+	hp -= clamp(damage-armor, 1.0, 999.0)
+	healthBar.max_value = maxhp
+	healthBar.value = hp
+	if hp <= 0:
+		death()
+
+func _on_ice_spear_timer_timeout():
+	icespear_ammo += icespear_baseammo + additional_attacks
+	iceSpearAttackTimer.start()
+
+
+func _on_ice_spear_attack_timer_timeout():
+	if icespear_ammo > 0:
+		var icespear_attack = iceSpear.instantiate()
+		icespear_attack.position = position
+		icespear_attack.target = get_random_target()
+		icespear_attack.level = icespear_level
+		add_child(icespear_attack)
+		icespear_ammo -= 1
+		if icespear_ammo > 0:
+			iceSpearAttackTimer.start()
+		else:
+			iceSpearAttackTimer.stop()
+
+func _on_vinyl_timer_timeout():
+	vinyl_ammo += vinyl_baseammo + additional_attacks
+	vinylAttackTimer.start()
+
+func _on_vinyl_attack_timer_timeout():
+	if vinyl_ammo > 0:
+		var vinyl_attack = vinyl.instantiate()
+		vinyl_attack.position = position
+		vinyl_attack.target = get_random_target()
+		vinyl_attack.level = vinyl_level
+		add_child(vinyl_attack)
+		vinyl_ammo -= 1
+		if vinyl_ammo > 0:
+			vinylAttackTimer.start()
+		else:
+			vinylAttackTimer.stop()
+
+func _on_tornado_timer_timeout():
+	tornado_ammo += tornado_baseammo + additional_attacks
+	tornadoAttackTimer.start()
+
+func _on_tornado_attack_timer_timeout():
+	if tornado_ammo > 0:
+		var tornado_attack = tornado.instantiate()
+		tornado_attack.position = position
+		tornado_attack.last_movement = last_movement
+		tornado_attack.level = tornado_level
+		add_child(tornado_attack)
+		tornado_ammo -= 1
+		if tornado_ammo > 0:
+			tornadoAttackTimer.start()
+		else:
+			tornadoAttackTimer.stop()
+
+func spawn_javelin():
+	var get_javelin_total = javelinBase.get_child_count()
+	var calc_spawns = (javelin_ammo + additional_attacks) - get_javelin_total
+	while calc_spawns > 0:
+		var javelin_spawn = javelin.instantiate()
+		javelin_spawn.global_position = global_position
+		javelinBase.add_child(javelin_spawn)
+		calc_spawns -= 1
+	#Upgrade Javelin
+	var get_javelins = javelinBase.get_children()
+	for i in get_javelins:
+		if i.has_method("update_javelin"):
+			i.update_javelin()
+
+func get_random_target():
+	if enemy_close.size() > 0:
+		return enemy_close.pick_random().global_position
+	else:
+		return Vector2.UP
+
+
+func _on_enemy_detection_area_body_entered(body):
+	if not enemy_close.has(body):
+		enemy_close.append(body)
+
+func _on_enemy_detection_area_body_exited(body):
+	if enemy_close.has(body):
+		enemy_close.erase(body)
+
+
+func _on_grab_area_area_entered(area):
+	if area.is_in_group("loot"):
+		area.target = self
+
+func _on_collect_area_area_entered(area):
+	if area.is_in_group("loot"):
+		var gem_exp = area.collect()
+		calculate_experience(gem_exp)
+
+func calculate_experience(gem_exp):
+	var exp_required = calculate_experiencecap()
+	collected_experience += gem_exp
+	if experience + collected_experience >= exp_required: #level up
+		collected_experience -= exp_required-experience
+		experience_level += 1
+		experience = 0
+		exp_required = calculate_experiencecap()
+		levelup()
+	else:
+		experience += collected_experience
+		collected_experience = 0
+	
+	set_expbar(experience, exp_required)
+
+func calculate_experiencecap():
+	var exp_cap = experience_level
+	if experience_level < 20:
+		exp_cap = experience_level*5
+	elif experience_level < 40:
+		exp_cap + 95 * (experience_level-19)*8
+	else:
+		exp_cap = 255 + (experience_level-39)*12
+		
+	return exp_cap
+		
+func set_expbar(set_value = 1, set_max_value = 100):
+	expBar.value = set_value
+	expBar.max_value = set_max_value
+
+func levelup():
+	sndLevelUp.play()
+	lblLevel.text = str("Level: ",experience_level)
+	var tween = levelPanel.create_tween()
+	tween.tween_property(levelPanel,"position",Vector2(220,50),0.2).set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_IN)
+	tween.play()
+	levelPanel.visible = true
+	var options = 0
+	var optionsmax = 3
+	while options < optionsmax:
+		var option_choice = itemOptions.instantiate()
+		option_choice.item = get_random_item()
+		upgradeOptions.add_child(option_choice)
+		options += 1
+	get_tree().paused = true
+
+func upgrade_character(upgrade):
+	match upgrade:
+		"icespear1":
+			icespear_level = 1
+			icespear_baseammo += 1
+		"icespear2":
+			icespear_level = 2
+			icespear_baseammo += 1
+		"icespear3":
+			icespear_level = 3
+		"icespear4":
+			icespear_level = 4
+			icespear_baseammo += 2
+		"tornado1":
+			tornado_level = 1
+			tornado_baseammo += 1
+		"tornado2":
+			tornado_level = 2
+			tornado_baseammo += 1
+		"tornado3":
+			tornado_level = 3
+			tornado_attackspeed -= 0.5
+		"tornado4":
+			tornado_level = 4
+			tornado_baseammo += 1
+		"vinyl1":
+			vinyl_level = 1
+			vinyl_baseammo += 1
+		"vinyl2":
+			vinyl_level = 2
+			vinyl_baseammo += 1
+		"vinyl3":
+			vinyl_level = 3
+			vinyl_attackspeed -= 0.5
+		"vinyl4":
+			vinyl_level = 4
+			vinyl_baseammo += 1
+		"javelin1":
+			javelin_level = 1
+			javelin_ammo = 1
+		"javelin2":
+			javelin_level = 2
+		"javelin3":
+			javelin_level = 3
+		"javelin4":
+			javelin_level = 4
+		"armor1","armor2","armor3","armor4":
+			armor += 1
+		"speed1","speed2","speed3","speed4":
+			movement_speed += 20.0
+		"tome1","tome2","tome3","tome4":
+			spell_size += 0.10
+		"scroll1","scroll2","scroll3","scroll4":
+			spell_cooldown += 0.05
+		"ring1","ring2":
+			additional_attacks += 1
+		"food":
+			hp += 20
+			hp = clamp(hp,0,maxhp)
+	adjust_gui_collection(upgrade)
+	attack()
+	var option_children = upgradeOptions.get_children()
+	for i in option_children:
+		i.queue_free()
+	upgrade_options.clear()
+	collected_upgrades.append(upgrade)
+	levelPanel.visible = false
+	levelPanel.position = Vector2(800,50)
+	get_tree().paused = false
+	calculate_experience(0)
+	
+func get_random_item():
+	var dblist = []
+	for i in UpgradeDb.UPGRADES:
+		if i in collected_upgrades: #Find already collected upgrades
+			pass
+		elif i in upgrade_options: #If the upgrade is already an option
+			pass
+		elif UpgradeDb.UPGRADES[i]["type"] == "item": #Don't pick food
+			pass
+		elif UpgradeDb.UPGRADES[i]["prerequisite"].size() > 0: #Check for PreRequisites
+			var to_add = true
+			for n in UpgradeDb.UPGRADES[i]["prerequisite"]:
+				if not n in collected_upgrades:
+					to_add = false
+			if to_add:
+				dblist.append(i)
+		else:
+			dblist.append(i)
+	if dblist.size() > 0:
+		var randomitem = dblist.pick_random()
+		upgrade_options.append(randomitem)
+		return randomitem
+	else:
+		return null
+
+func change_time(argtime = 0):
+	time = argtime
+	var get_m = int(time/60.0)
+	var get_s = time % 60
+	if get_m < 10:
+		get_m = str(0,get_m)
+	if get_s < 10:
+		get_s = str(0,get_s)
+	lblTimer.text = str(get_m,":",get_s)
+
+func adjust_gui_collection(upgrade):
+	var get_upgraded_displayname = UpgradeDb.UPGRADES[upgrade]["displayname"]
+	var get_type = UpgradeDb.UPGRADES[upgrade]["type"]
+	if get_type != "item":
+		var get_collected_displaynames = []
+		for i in collected_upgrades:
+			get_collected_displaynames.append(UpgradeDb.UPGRADES[i]["displayname"])
+		if not get_upgraded_displayname in get_collected_displaynames:
+			var new_item = itemContainer.instantiate()
+			new_item.upgrade = upgrade
+			match get_type:
+				"weapon":
+					collectedWeapons.add_child(new_item)
+				"upgrade":
+					collectedUpgrades.add_child(new_item)
+
+func death():
+	deathPanel.visible = true
+	emit_signal("playerdeath")
+	get_tree().paused = true
+	var tween = deathPanel.create_tween()
+	tween.tween_property(deathPanel,"position",Vector2(220,50),3.0).set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
+	tween.play()
+	if time >= 300:
+		lblResult.text = "You Win"
+		sndVictory.play()
+	else:
+		lblResult.text = "You Lose"
+		sndLose.play()
+
+
+func _on_btn_menu_click_end():
+	get_tree().paused = false
+	var _level = get_tree().change_scene_to_file("res://TitleScreen/menu.tscn")
