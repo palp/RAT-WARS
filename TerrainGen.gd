d8488c884bc39b218949ad44dfcf6bb2104fcfdc
Luke <larson.luke.t@gmail.com>
2023-11-29 23:31:48 -0600

    Just getting started here.

---
+++ TerrainGen.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+@export var terrain_upper_bound_piece := Node2D
+@export var terrain_upper_piece := Node2D
+@export var terrain_center_piece := Node2D
+@export var terrain_lower_piece := Node2D
+@export var terrain_lower_bound_piece := Node2D
+
+@export var clutter_piece_one := Node2D
+@export var clutter_piece_two := Node2D
+@export var clutter_piece_three := Node2D
+@export var clutter_piece_four := Node2D
+@export var clutter_piece_five := Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
