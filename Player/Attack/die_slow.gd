2547f0767d658797fa2010b4cf69ec944c3242e2
wolf-paper <eleanor@kaze.be>
2023-11-30 17:47:19 -0500

    committing to remote to prepare for merge

---
+++ Player/Attack/die_slow.gd
@@ -0,0 +1,34 @@
+extends Area2D
+
+var level = 1
+var hp = 999
+var speed = 150.0
+var damage = 0
+var attack_size = 1.0
+var knockback_amount = 0
+
+var target = Vector2.ZERO
+
+var angle = Vector2.ZERO
+
+var puddle = preload("res://Player/Attack/die_slow_puddle.tscn")
+
+@onready var player = get_tree().get_first_node_in_group("player")
+@onready var sprite = $Sprite2D
+@onready var collision = $CollisionShape2D
+
+func _physics_process(delta):
+	if angle.angle() >= 0:
+		rotation += .08
+	if angle.angle() < 0:
+		rotation -= .08
+	position += angle.normalized() * speed * delta
+
+
+func _on_flight_duration_timer_timeout():
+	sprite.hide()
+	var puddle_attack = puddle.instantiate()
+	puddle_attack.position = position
+	puddle_attack.level = level
+	add_child(puddle_attack)
+	
