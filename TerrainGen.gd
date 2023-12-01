ae606e250d6a634065c49457030b0034ccc52705
Luke <larson.luke.t@gmail.com>
2023-12-01 00:09:12 -0600

    Terrain prefabs exist now

--- TerrainGen.gd
+++ TerrainGen.gd
@@ -1,16 +1,6 @@
 extends Node2D
 
-@export var terrain_upper_bound_piece := Node2D
-@export var terrain_upper_piece := Node2D
-@export var terrain_center_piece := Node2D
-@export var terrain_lower_piece := Node2D
-@export var terrain_lower_bound_piece := Node2D
-
-@export var clutter_piece_one := Node2D
-@export var clutter_piece_two := Node2D
-@export var clutter_piece_three := Node2D
-@export var clutter_piece_four := Node2D
-@export var clutter_piece_five := Node2D
+@export var terrain_packedscene := Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
