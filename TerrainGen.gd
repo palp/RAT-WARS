a827103b2cdbc20491e2da0f3d2c7d8bfe5344f3
Luke <larson.luke.t@gmail.com>
2023-12-02 13:05:42 -0600

    Infinite scrolling (#79)
    * Terrain Tiles Move!
    
    However, they don't move to where they're supposed to. I'll fix that in the morning.
    
    * Scrolling Works Now
    
    Level scrolling now goes on for as long as the engine doesn't cap our movement

--- TerrainGen.gd
+++ TerrainGen.gd
@@ -5,6 +5,11 @@ extends Node2D
 @export var packedscene_terrain_vertical := PackedScene
 @export var packedscene_terrain_clutter := PackedScene
 
+# a reference to the player so we can keep track of where they are
+@export var player := Node2D
+var half_camera_viewport_size : int
+var furthest_prefab
+
 # these are gonna instantiate our packedscenes
 var level_horizontal
 var level_vertical
@@ -12,6 +17,9 @@ var level_clutter
 
 # keep track of the current size of our level, so we know how far to move tiles later
 var current_size : Vector2
+
+# keep track of where "zero" is relative to where the prefabs are. Should always be the position of the middle prefab.
+var current_zero : Vector2
 
 # What types of things are active in this instance of TerrainGen?
 @export var scroll_horizontal : bool
@@ -25,6 +33,9 @@ var clutter_array
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# grab the size of the camera viewport and chop it in half for later reference
+	half_camera_viewport_size = get_viewport().size.x / 2
+	
 	# temporary variable because we need something to hold our scene when we load it
 	var temp_scene
 	if (scroll_horizontal):
@@ -59,7 +70,21 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if (scroll_horizontal):
+		var prefab_to_move
+		var closest_prefab
+		if (player.position.x > 0.0 && player.position.x >= current_zero.x + (current_size.x / 2 - half_camera_viewport_size)):
+			prefab_to_move = get_furthest_prefab()
+			closest_prefab = get_closest_prefab()
+			prefab_array_horizontal[prefab_to_move].position.x += prefab_array_horizontal[prefab_to_move].position.distance_to(prefab_array_horizontal[closest_prefab].position) + get_prefab_length(prefab_to_move)
+			current_zero = prefab_array_horizontal[get_furthest_prefab()].position
+			current_zero.x -= current_size.x / prefab_array_horizontal.size()
+		elif (player.position.x < 0.0 && player.position.x <= current_zero.x - (current_size.x / 2 - half_camera_viewport_size)):
+			prefab_to_move = get_furthest_prefab()
+			closest_prefab = get_closest_prefab()
+			prefab_array_horizontal[prefab_to_move].position.x -= prefab_array_horizontal[prefab_to_move].position.distance_to(prefab_array_horizontal[closest_prefab].position) + get_prefab_length(prefab_to_move)
+			current_zero = prefab_array_horizontal[get_furthest_prefab()].position
+			current_zero.x += current_size.x / prefab_array_horizontal.size()
 	
 # Iterate through all in-use scene arrays and start arranging them
 func build_level():
@@ -77,10 +102,30 @@ func build_level():
 			if (n > center_node):
 				temp_position = prefab_array_horizontal[center_node].get_used_rect().size.x / 2 + prefab_array_horizontal[n].get_used_rect().size.x / 2
 				prefab_array_horizontal[n].position.x += temp_position * prefab_array_horizontal[n].get_scale().x * 64
-				current_size.x += prefab_array_horizontal[n].get_used_rect().size.x
+				current_size.x += prefab_array_horizontal[n].get_used_rect().size.x * prefab_array_horizontal[n].get_scale().x * 64
 			# and if the index of this prefab is less than the center, move it to the left
 			elif (n < center_node):
 				temp_position = prefab_array_horizontal[center_node].get_used_rect().size.x / 2 + prefab_array_horizontal[n].get_used_rect().size.x / 2
 				prefab_array_horizontal[n].position.x -= temp_position * prefab_array_horizontal[n].get_scale().x * 64
-				current_size.x += prefab_array_horizontal[n].get_used_rect().size.x
-			
+				current_size.x += prefab_array_horizontal[n].get_used_rect().size.x * prefab_array_horizontal[n].get_scale().x * 64
+		current_zero = prefab_array_horizontal[center_node].position
+
+func get_furthest_prefab():
+	var distance_array = []
+	
+	for n in prefab_array_horizontal.size():
+		distance_array.append(player.position.distance_to(prefab_array_horizontal[n].position))
+		
+	return distance_array.find(distance_array.max())
+	
+func get_closest_prefab():
+	var distance_array = []
+	
+	for n in prefab_array_horizontal.size():
+		distance_array.append(player.position.distance_to(prefab_array_horizontal[n].position))
+		
+	return distance_array.find(distance_array.min())
+	
+func get_prefab_length(prefab_index : int):
+	var prefab_length = prefab_array_horizontal[prefab_index].get_used_rect().size.x * prefab_array_horizontal[prefab_index].get_scale().x * 64
+	return prefab_length
