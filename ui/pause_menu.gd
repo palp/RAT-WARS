68b5baa2e36b5c279bf6e4efd306c644688cb71d
Stephan Auerhahn <palp@palp.org>
2023-12-02 15:02:56 -0800

    Godot 4.2 (#89)
    * fixing runtime errors
    
    * updating to 4.2
    
    * update export
    
    * testing export
    
    * test with 4.2
    
    * renderer back
    
    * continue using old gdunit

--- ui/pause_menu.gd
+++ ui/pause_menu.gd
@@ -6,5 +6,6 @@ func _on_options_pressed():
 	options_menu.visible = !options_menu.visible
 
 func _on_main_menu_pressed():
-	get_tree().change_scene_to_file("res://demo.tscn")
 	get_tree().paused = false
+	get_tree().change_scene_to_file("res://demo.tscn")
+	
