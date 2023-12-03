028fd57b1f59a598f5216ab99d03b9a9078f0b7f
Stephan Auerhahn <palp@palp.org>
2023-12-02 23:38:28 -0800

    update gdunit to 4.2 (#111)
    * update gdunit to 4.2
    
    * fix tests
    
    * clean up old stuff
    
    * fix attack manager tests
    
    * ignore vscode

--- test/Player/Attack/attack_manager_test.gd
+++ test/Player/Attack/attack_manager_test.gd
@@ -10,12 +10,12 @@ const __source = 'res://Player/Attack/attack_manager.gd'
 
 # Just a basic test of spawning for now
 func test_process() -> void:
-	var attack_manager = auto_free(load(__source).new()) as attack_manager
-	attack_manager.attacker = mock(Player)
-	attack_manager.attacker.hp = 100
-	attack_manager._process(100)
-	assert_array(attack_manager.get_children()).is_empty()
-	attack_manager.attacks['vinyl'].level = 1
-	attack_manager.attacks['vinyl'].base_ammo = 1
-	attack_manager._process(100)
-	assert_array(attack_manager.get_children()).is_not_empty()
+	var manager = auto_free(load(__source).new()) as attack_manager
+	manager.attacker = mock(Player)
+	manager.attacker.hp = 100
+	manager._process(100)
+	assert_array(manager.get_children()).is_empty()
+	manager.attacks['vinyl'].level = 1
+	manager.attacks['vinyl'].base_ammo = 1
+	manager._process(100)
+	assert_array(manager.get_children()).is_not_empty()
