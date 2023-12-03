ca33ed97877f982fa11ada436226fa6ebd25f2b0
Stephan Auerhahn <palp@palp.org>
2023-12-02 20:19:31 -0800

    stop music on win/lose (#108)

--- Audio/Music/snd_Music.gd
+++ Audio/Music/snd_Music.gd
@@ -1,9 +1,15 @@
 extends AudioStreamPlayer
 
+var stopped = false
 
 func _on_player_playerdeath():
+	stopped = true
 	playing = false
 
+func _on_player_playervictory():
+	stopped = true
+	playing = false
 
 func _on_finished():
-	play()
+	if not stopped:
+		play()
