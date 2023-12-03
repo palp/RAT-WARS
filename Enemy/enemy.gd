e60b2e3e11351c49390d2042411e30b96e82f76d
Stephan Auerhahn <palp@palp.org>
2023-12-03 14:28:36 -0800

    Make the game end after bsss explodes, not on death (#123)

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -56,15 +56,15 @@ func _physics_process(_delta):
 func death():
 	emit_signal("remove_from_array",self)
 	var enemy_death = death_anim.instantiate()
+	if trigger_victory:
+		enemy_death.connect("tree_exited", player.victory)
 	enemy_death.scale = sprite.scale
 	enemy_death.global_position = global_position
 	get_parent().call_deferred("add_child",enemy_death)
 	var new_gem = exp_gem.instantiate()
 	new_gem.global_position = global_position
 	new_gem.experience = experience
-	loot_base.call_deferred("add_child",new_gem)
-	if trigger_victory:
-		player.victory()
+	loot_base.call_deferred("add_child",new_gem)	
 	queue_free()
 
 func _on_hurt_box_hurt(damage, angle, knockback_amount):
