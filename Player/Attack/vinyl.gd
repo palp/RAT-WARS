6e23f017654123a37150253cb1c319c8092f9dba
Stephan Auerhahn <stephan@stability.ai>
2023-12-05 01:27:43 -0800

    reduce vinyl damage at higher levels

--- Player/Attack/vinyl.gd
+++ Player/Attack/vinyl.gd
@@ -3,7 +3,7 @@ extends Area2D
 var level = 1
 var hp = 999
 var speed = 150.0
-var damage = 4
+var damage = 3
 var attack_size = 1.0
 var knockback_amount = 100
 
@@ -29,25 +29,25 @@ func _ready():
 		1:
 			hp = 9999
 			speed = 150.0
-			damage = 4
+			damage = 3
 			knockback_amount = 100
 			attack_size = 1.0
 		2:
 			hp = 9999
 			speed = 150.0
-			damage = 8
+			damage = 6
 			knockback_amount = 100
 			attack_size = 1.0
 		3:
 			hp = 9999
 			speed = 150.0
-			damage = 10
+			damage = 8
 			knockback_amount = 100
 			attack_size = 1.0
 		4:
 			hp = 9999
 			speed = 150.0
-			damage = 12
+			damage = 10
 			knockback_amount = 125
 			attack_size = 1.0
 
