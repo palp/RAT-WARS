0ae25f16b236dfc6ac041c37949410294ee0a138
toaster <midnighttokyocarnage@icloud.com>
2023-12-07 05:32:21 +0500

    TODO, find an alternative fit for the currtent scene setup. shouldn't be that hard

--- LaunchSequence/CutsceneNode/cutscene.gd
+++ LaunchSequence/CutsceneNode/cutscene.gd
@@ -2,11 +2,18 @@ extends Node
 
 @onready var animation_control = $AnimationPlayer
 @onready var text = $RichTextLabel
+@onready var control_node = $Control
+@export var scene_dialog_path = ""
+@export var text_speed = 0.05
+var dialog 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	text.get_node("Timer")
+	pass 
 
+# TODO https://www.youtube.com/watch?v=GzPvN5wsp7Y&t=185s
+# FIX outdated 👆
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
