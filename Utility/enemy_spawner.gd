493a2245a8cc26577aea0da037f96097eff4f573
Stephan Auerhahn <stephan@stability.ai>
2023-12-07 22:50:49 -0800

    smaller spawn area and less spawns

--- Utility/enemy_spawner.gd
+++ Utility/enemy_spawner.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 @export var spawns: Array[Spawn_info] = []
 @export var spawn_area: Rect2 = Rect2(0,0,0,0)
+@export var no_spawn_area: Rect2 = Rect2(0,0,0,0)
 @export var spawn_cap = 0
 @export var track_vertical = false
 @export var track_horizontal = false
@@ -20,9 +21,12 @@ var enemy_counter = 0
 func spawn_enemies():
 	var enemy_spawns = spawns
 	if track_vertical:
-		spawn_area.position.y = player.global_position.y
+		spawn_area.position.y = player.global_position.y		
 	if track_horizontal:
 		spawn_area.position.x = player.global_position.x	
+	# "no spawn area" always tracks
+	no_spawn_area.position = player.global_position
+		
 
 	for i in enemy_spawns:
 		if time >= i.time_start and time <= i.time_end:
@@ -37,6 +41,9 @@ func spawn_enemies():
 					enemies_to_spawn = min(i.enemy_num, max(spawn_cap - enemy_counter, 0))
 				while counter < enemies_to_spawn:
 					var random_position = get_random_position()
+					if no_spawn_area.get_area() != 0:
+						while no_spawn_area.has_point(random_position):
+							random_position = get_random_position()
 					var enemy_spawn = new_enemy.instantiate()
 					enemy_spawn.global_position = random_position
 					enemy_spawn.top_level = true
@@ -120,5 +127,5 @@ func get_random_position():
 			spawn_pos2 = bottom_left
 	
 	var x_spawn = randf_range(spawn_pos1.x, spawn_pos2.x)
-	var y_spawn = randf_range(spawn_pos1.y,spawn_pos2.y)
+	var y_spawn = randf_range(spawn_pos1.y,spawn_pos2.y)	
 	return Vector2(x_spawn,y_spawn)
