37e0bcfda0f02c30d472d3346dea38c38b7f1c01
toaster <midnighttokyocarnage@icloud.com>
2023-12-09 05:39:30 +0500

    Cutscene data folder added

--- LaunchSequence/ScreenFadeInAndOut/screen_fade_in_and_out.gd
+++ LaunchSequence/ScreenFadeInAndOut/screen_fade_in_and_out.gd
@@ -1,11 +1,29 @@
 extends Node2D
 
-@onready var player = $AnimationPlayer
+signal transition_in_progress(boolean)
+
+@onready var player: AnimationPlayer = $AnimationPlayer
+@onready var anim_type = "type_1"
+@onready var in_progress: bool = false
 
 func fadeToBlack(_animation_name = "type_1"):
 	player.play(_animation_name)
 	pass
 
-func fadeToScreen(_animation_name):
+func fadeToScreen(_animation_name = "type_1"):
 	player.play_backwards(_animation_name)
 	pass
+
+func _ready():
+	# fadeToScreen(anim_type)
+	pass
+
+func _on_animation_finished(anim_name:StringName):
+	in_progress = false
+	emit_signal("transition_in_progress", in_progress)
+	pass # Replace with function body.
+
+func _on_animation_started(anim_name:StringName):
+	in_progress = true
+	emit_signal("transition_in_progress", in_progress)
+	pass # Replace with function body.
