4104bcd84a4e407a22abc3e4dbce46d490ebe341
toaster <midnighttokyocarnage@icloud.com>
2023-12-09 09:36:34 +0500

    Added Music

--- LaunchSequence/CutsceneNode/cutscene.gd
+++ LaunchSequence/CutsceneNode/cutscene.gd
@@ -1,6 +1,6 @@
 extends Node
 
-enum CutsceneState{begin, transition_to_black, transition_to_screen, transition_frame, text_printing, idle, end}
+enum CutsceneState{begin, transition_to_black, transition_to_screen, transition_frame, text_printing, fade_to_black, idle, end}
 
 # TODO connnect to something up the hierary. 
 signal cutscene_finished()
@@ -11,7 +11,9 @@ signal cutscene_finished()
 @onready var indicator_next: AnimatedSprite2D = get_node("ButtonNext/AnimatedSprite2D")
 @onready var cutscene_frame: Sprite2D = get_node("CutsceneFrame")
 @onready var screen_fade_in_out: Node2D = get_node("ScreenFadeInAndOut")
-@export var scene_text_path = ""
+@onready var animationPlayer: AnimationPlayer = get_node("AnimationPlayer")
+@onready var music_stream: AudioStreamPlayer = get_node("AudioStreamPlayer")
+@export var scene_title = ""
 @export var text_speed = 0.02
 @export var frame_transition_speed: float = 1.2
 @onready var indicator_skip: Sprite2D = get_node("ButtonSkip/Sprite")
@@ -22,6 +24,7 @@ var text_array = []
 var img_array = []
 @onready var slide_image: Sprite2D = get_node("SlideImage")
 @onready var ignore_player_input: bool = true
+@export var music_format = ""
 
 # TODO There needs to be a way to provide an instances cutscene node with
 # The title of a requested cutscene
@@ -32,17 +35,36 @@ var current_scene_state: CutsceneState
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	text_node.text = ""
 	string_index = 0
 	img_index = 0
 	indicator_next.visible = false
 	skip_visible = false
 	current_scene_state = CutsceneState.begin
-	img_array = get_scene_slides("opening")
-	text_array = get_scene_text("opening")
-	text_processing(text_array[string_index])
+
 	Log.info("Cutscene node ready")	
 	pass 
 
+func launch_scene():
+	img_array = get_slides_array("opening")
+	text_array = get_scene_text("opening")
+	get_music(music_format, scene_title)
+	test_text_array(text_array)
+	test_img_array(img_array)
+
+	text_processing(text_array[string_index])
+	pass
+func test_text_array(array):
+	for s in array:
+		Log.info(s)
+		pass
+	pass
+func test_img_array(array: Array):
+	for n in array.size():
+		Log.info(array[n].resource_name)
+		Log.info(array[n].resource_path)
+		pass
+	pass
 	# * NODE SETUP
 
 # * This section is all about loadign the cutscene text_node from json file
@@ -67,7 +89,7 @@ func get_scene_text(cutscene_name) -> Array:
 
 # * Loading Images into the declared array
 # Loads a correct set of slides for (Sprite2D)SlideImage object
-func get_scene_slides(cutscene_name) -> Array:
+func get_slides_array(cutscene_name) -> Array:
 	var cutscene_slides_path = "res://CutsceneData/Slides/"+cutscene_name
 	var image_array = []
 	# Open the pics directory for provided cutscene name and prepare them
@@ -85,10 +107,15 @@ func get_scene_slides(cutscene_name) -> Array:
 	return image_array
 
 # This is meant to be called from inside the script/self
-func show_next_slide(next_slide):
+func show_next_slide(next_slide: Resource):
+	Log.info("show next slide")
 	slide_image.texture = next_slide
 	pass
 
+		# * Load Music
+func get_music(format: String, cutscene_name: String):
+	music_stream.stream = load("res://CutsceneData/Music/" + cutscene_name +"." + format)
+	pass
 		# * SIGNAL PROCESSING
 
 	#// ? Why are there funcitions in this section, that start both with "_on..." and "on"?
@@ -97,8 +124,11 @@ func show_next_slide(next_slide):
 
 # * From Slide Image Node
 func _on_slide_changed():
+	Log.info("_on_slide_changed")
 	match current_scene_state:
 		CutsceneState.transition_to_screen:
+			current_scene_state = CutsceneState.transition_frame
+			cutscene_frame.frame = 0
 			screen_fade_in_out.fadeToScreen()
 			pass
 
@@ -117,32 +147,38 @@ func _on_transition_in_progress(in_progress: bool):
 			
 # Called when fade out animation is finished
 func on_screen_transition_finished():
-	Log.info("should be called also")
+	Log.info("scene state: " + str(current_scene_state))
 	match current_scene_state:
-		CutsceneState.transition_to_screen:
+		CutsceneState.transition_to_screen, CutsceneState.transition_frame:
 			Log.info("should be called also also")
 			ignore_player_input = false
 			current_scene_state = CutsceneState.transition_frame
 			var tween = get_tree().create_tween()
 			tween.tween_interval(frame_transition_speed)
 			tween.tween_property(cutscene_frame, "frame", 7, frame_transition_speed)
-			tween.tween_interval(frame_transition_speed)
+			# tween.tween_interval(frame_transition_speed)
 			tween.tween_callback(on_cutscene_frame_up) 
 			# show cutscene frame
 		CutsceneState.transition_to_black:
+			text_node.visible_characters = 0
+			img_index = img_index + 1
 			current_scene_state = CutsceneState.transition_to_screen
-			img_index =+ 1
-			show_next_slide(img_index)
+			show_next_slide(img_array[img_index])
 			pass
 		CutsceneState.text_printing:
 			pass
+		CutsceneState.fade_to_black:
+			slide_image.visible = false
+			screen_fade_in_out.hide_visual()
+			text_processing(text_array[string_index])
+			pass
 		CutsceneState.end:
+			Log.info("Queue free cutscene")
 			queue_free()
 			pass
 
 # * From CutscenFrame
 func on_cutscene_frame_up():
-	current_scene_state = CutsceneState.text_printing
 	text_processing(text_array[string_index])
 	# begin printing text_node
 	pass
@@ -150,11 +186,20 @@ func on_cutscene_frame_up():
 		# * Processing text_node extracted from the .json file
 # * Check the current string in the array fo commandz
 func text_processing(string_from_array):
+	string_index = string_index + 1
+	Log.info("current string index: " + str(string_index))
 	match string_from_array:
+		"ENDSCENE":
+			current_scene_state = CutsceneState.end
+			screen_fade_in_out.fadeToBlack()
 		"SHOWSLIDE":
 			text_command_showslide()
+		"FADETOBLACK":
+			current_scene_state = CutsceneState.fade_to_black
+			screen_fade_in_out.fadeToBlack()
 		_:
 			# default case is a block of text_node that is to be printed out
+			ignore_player_input = false
 			print_text(string_from_array)
 
 # Called if a string says "SHOWSLIDE"
@@ -167,8 +212,8 @@ func text_command_showslide():
 			show_next_slide(img_array[img_index])
 			# $ScreenFadeInAndOut.fadeToScreen()
 		CutsceneState.idle:
-			ignore_player_input = true
 			current_scene_state = CutsceneState.transition_to_black
+			ignore_player_input = true
 			screen_fade_in_out.fadeToBlack()
 			pass
 			
@@ -191,7 +236,6 @@ func _on_text_speed_timer_timeout():
 		indicator_next.visible = true
 		ignore_player_input = false
 		current_scene_state = CutsceneState.idle
-		string_index =+ 1
 		pass
 
 func print_whole_text(string):
@@ -201,10 +245,10 @@ func print_whole_text(string):
 	indicator_next.play("default")
 	indicator_next.visible = true
 	current_scene_state = CutsceneState.idle
-	string_index =+ 1
 	pass
 
-# TODO loading and playing music
+		# * Music Processing
+		# ??
 
 		# * Processing player's input
 # Input from "Next" button in the lower right corner
@@ -214,9 +258,9 @@ func _input_next_button(event: InputEvent):
 	if event is InputEventMouseButton and event.is_pressed():
 		if current_scene_state == CutsceneState.text_printing:
 			text_node.get_node("TextSpeedTimer").stop()
-			print_whole_text(text_array[string_index])
+			print_whole_text(text_node.text)
 		elif current_scene_state == CutsceneState.idle:
-			string_index = string_index + 1
+			indicator_next.visible = false
 			text_processing(text_array[string_index])
 			pass
 
@@ -234,6 +278,7 @@ func _input_skip_button(event: InputEvent):
 	# current_scene_state = CutsceneState.end
 	# Queue_free this bitch
 	pass
+
 func show_skip():
 	match skip_visible:
 		true:
@@ -247,7 +292,7 @@ func show_skip():
 			var tween = get_tree().create_tween()
 			tween.tween_property(indicator_skip, "position", Vector2(indicator_skip.position.x, 24), 0.25)
 			tween.tween_interval(0.25)
-			tween.tween_callback(indicator_next.get_node("ButtonSkipHideTimer").start)
+			tween.tween_callback(get_node("ButtonSkip/ButtonSkipHideTimer").start.bind(2))
 			pass
 # On json file contents
 # i decided i'm going to use special commands embeded as strings to instruct node to show 
--- LaunchSequence/ScreenFadeInAndOut/screen_fade_in_and_out.gd
+++ LaunchSequence/ScreenFadeInAndOut/screen_fade_in_and_out.gd
@@ -10,20 +10,24 @@ func fadeToBlack(_animation_name = "type_1"):
 	player.play(_animation_name)
 	pass
 
+func hide_visual():
+	$Sprite2D.frame = 0
+	pass
+
 func fadeToScreen(_animation_name = "type_1"):
 	player.play_backwards(_animation_name)
 	pass
 
 func _ready():
-	# fadeToScreen(anim_type)
+	$Sprite2D.frame = 8
 	pass
 
-func _on_animation_finished(anim_name:StringName):
+func _on_animation_finished(_anim_name:StringName):
 	in_progress = false
 	emit_signal("transition_in_progress", in_progress)
 	pass # Replace with function body.
 
-func _on_animation_started(anim_name:StringName):
+func _on_animation_started(_anim_name:StringName):
 	in_progress = true
 	emit_signal("transition_in_progress", in_progress)
 	pass # Replace with function body.
