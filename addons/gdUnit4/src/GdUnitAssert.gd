f93b390f30c0e4c52377e1155adee2610dd8acae
Stephan Auerhahn <stephan@stability.ai>
2025-02-06 02:49:16 -0800

    update to godot 4.3

--- addons/gdUnit4/src/GdUnitAssert.gd
+++ addons/gdUnit4/src/GdUnitAssert.gd
@@ -3,59 +3,47 @@ class_name GdUnitAssert
 extends RefCounted
 
 
-# Scans the current stack trace for the root cause to extract the line number
-static func _get_line_number() -> int:
-	var stack_trace := get_stack()
-	if stack_trace == null or stack_trace.is_empty():
-		return -1
-	for stack_info in stack_trace:
-		var function :String = stack_info.get("function")
-		# we catch helper asserts to skip over to return the correct line number
-		if function.begins_with("assert_"):
-			continue
-		if function.begins_with("test_"):
-			return stack_info.get("line")
-		var source :String = stack_info.get("source")
-		if source.is_empty() \
-			or source.begins_with("user://") \
-			or source.ends_with("GdUnitAssert.gd") \
-			or source.ends_with("AssertImpl.gd") \
-			or source.ends_with("GdUnitTestSuite.gd") \
-			or source.ends_with("GdUnitSceneRunnerImpl.gd") \
-			or source.ends_with("GdUnitObjectInteractions.gd") \
-			or source.ends_with("GdUnitAwaiter.gd"):
-			continue
-		return stack_info.get("line")
-	return -1
-
-
 ## Verifies that the current value is null.
+@warning_ignore("untyped_declaration")
 func is_null():
 	return self
 
 
 ## Verifies that the current value is not null.
+@warning_ignore("untyped_declaration")
 func is_not_null():
 	return self
 
 
 ## Verifies that the current value is equal to expected one.
 @warning_ignore("unused_parameter")
-func is_equal(expected):
+@warning_ignore("untyped_declaration")
+func is_equal(expected: Variant):
 	return self
 
 
 ## Verifies that the current value is not equal to expected one.
 @warning_ignore("unused_parameter")
-func is_not_equal(expected):
+@warning_ignore("untyped_declaration")
+func is_not_equal(expected: Variant):
 	return self
 
 
-func test_fail():
+@warning_ignore("untyped_declaration")
+func do_fail():
 	return self
 
 
 ## Overrides the default failure message by given custom message.
 @warning_ignore("unused_parameter")
+@warning_ignore("untyped_declaration")
 func override_failure_message(message :String):
+	return self
+
+
+## Appends a custom message to the failure message.
+## This can be used to add additional infromations to the generated failure message.
+@warning_ignore("unused_parameter")
+@warning_ignore("untyped_declaration")
+func append_failure_message(message :String):
 	return self
